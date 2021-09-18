93228dc4659403fe2852d47df9e0d4323178d238
uky222 <72603675+uky222@users.noreply.github.com>
2021-09-18 12:28:32 +0530

    Update start.py

--- start.py
+++ start.py
@@ -1,4 +1,5 @@
 from os import system
-system ("git clone https://GitHub.com/ULTRA-OP/ULTRA-X && cd ULTRA-X && python3 -m ULTRA")
+system ("git clone https://GitHub.com/uky222/Batch-X && cd Batch-X && python3 -m ULTRA")
 # try af
 print ("YOU GOT ERROR SAVE THE LOGS AND GIVE THIS LOGS TO @UltraXchaT")
+#https://github.com/uky222/Batch-X
